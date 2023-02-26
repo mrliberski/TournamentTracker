cf431e8e3214a09b6e17c7f4a81c3226c98cbe43
Pawel Liberski <panliberski@outlook.com>
2023-02-26 19:53:36 +0000

    adding class outline for mechanics model

--- TrackerLibrary/TeamModel.cs
+++ TrackerLibrary/TeamModel.cs
@@ -9,6 +9,7 @@ namespace TrackerLibrary
     public class TeamModel
     {
         public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+
         public string TeamName { get; set; }
 
     }
