2a732cf5e6cbed3d644ce340cf60bdce87f32bce
Pawel Liberski <panliberski@outlook.com>
2023-03-04 00:26:10 +0000

    Interface draft added

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitializeConnections(bool database, bool textFiles)
+        {
+            if (database)
+            {
+                // TODO - if database
+            }
+
+            if (textFiles)
+            {
+                //TODO - if textfile
+            }
+        }
+
+    }
+}
